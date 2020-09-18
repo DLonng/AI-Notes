5c7aa5999a82a4222d84656789e033ece50c2449
dlonng <dlonnng@gmail.com>
2020-09-18 22:56:48 +0800

    remove code

--- SemanticMap/octomap_generator_ws/src/octomap_generator/src/semantic_octree/semantics_max.cpp
+++
@@ -1,8 +0,0 @@
-#include<semantics_octree/semantics_max.h>
-namespace octomap
-{
-  std::ostream& operator<<(std::ostream& out, SemanticsMax const& s)
-  {
-    return out << '(' << s.semantic_color << ", " << s.confidence << ')';
-  }
-}
--- SemanticMap/octomap_generator_ws/src/scout_msgs/CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-# Changelog for package scout_msgs
-
-## 0.0.1 (2019-06-14)
-------------------
-* Initial development of scout_msgs for Scout
-* Contributors: Ruixiang Du
